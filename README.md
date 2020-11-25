101206d0e5442e1897cf538af0fb5819eaa4a960
TheGiddyLimit <TheGiddyLimit@5etools.com>
2020-11-25 23:38:38 +0000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Googly Eyes
+
+### Usage
+
+Click the small Googly Eye button in the top-right corner of the canvas. With Googly Eye mode enabled, you can now:
+  - Left-click on a token to add a googly eye
+  - Right-click on a googly eye to remove it
---
+++ js/GooglyEyes.js
@@ -0,0 +1,183 @@
+class GooglyEyes {
+	static async pInit () {
+		await this._pInitTexture();
+		this._initToggle();
+		this._initTokenMods();
+	}
+
+	static async _pInitTexture () {
+		GooglyEyes._TEXTURE = await loadTexture(`modules/${GooglyEyes._MODULE_NAME}/media/googly-eye.png`);
+	}
+
+	static _initToggle () {
+		const $btn = $(`<button class="ggly__btn" title="Toggle Google Eye Tool"></button>`)
+			.click(() => {
+				$btn.toggleClass("ggly__btn--active");
+				GooglyEyes._IS_ACTIVE = !GooglyEyes._IS_ACTIVE;
+			})
+			.appendTo(document.body);
+	}
+
+	static _initTokenMods () {
+		const cachedRefresh = Token.prototype.refresh;
+		const cachedClickLeft = Token.prototype._onClickLeft;
+		const cachedClickRight = Token.prototype._onClickRight;
+		const cachedClickLeft2 = Token.prototype._onClickLeft2;
+		const cachedClickRight2 = Token.prototype._onClickRight2;
+
+		Token.prototype.refresh = function (...args) {
+			GooglyEyes._doRender(this);
+			return cachedRefresh.bind(this)(...args);
+		};
+
+		Token.prototype._onClickLeft = function (...args) {
+			if (GooglyEyes._doHandleClickLeft(this, ...args)) return false;
+			return cachedClickLeft.bind(this)(...args);
+		};
+
+		Token.prototype._onClickLeft2 = function (...args) {
+			if (GooglyEyes._doHandleClickLeft(this, ...args)) return false;
+			return cachedClickLeft2.bind(this)(...args);
+		};
+
+		Token.prototype._onClickRight = function (...args) {
+			if (GooglyEyes._doHandleClickRight(this, ...args)) return false;
+			return cachedClickRight.bind(this)(...args);
+		};
+
+		Token.prototype._onClickRight2 = function (...args) {
+			if (GooglyEyes._doHandleClickRight(this, ...args)) return false;
+			return cachedClickRight2.bind(this)(...args);
+		};
+
+		this._doTriggerExistingTokens();
+	}
+
+	static _doTriggerExistingTokens () {
+		(canvas?.tokens?.placeables || []).forEach(tk => {
+			try {
+				tk.mouseInteractionManager.callbacks.clickLeft = tk._onClickLeft.bind(tk);
+				tk.mouseInteractionManager.callbacks.clickRight = tk._onClickRight.bind(tk);
+				tk.mouseInteractionManager.callbacks.clickLeft2 = tk._onClickLeft.bind(tk);
+				tk.mouseInteractionManager.callbacks.clickRight2 = tk._onClickRight.bind(tk);
+				tk.mouseInteractionManager._activateClickEvents();
+				tk.refresh();
+			} catch (e) {
+				// Sanity check/should never occur
+				console.warn(`Failed to refresh token "${tk._id}"!`, e);
+			}
+		});
+	}
+
+	static _doRender (token) {
+		const seenKeys = new Set();
+
+		(token.data.flags?.[GooglyEyes._MODULE_NAME]?.points || [])
+			.forEach(pt => {
+				const eyeKey = `${pt.x}-${pt.y}`;
+				seenKeys.add(eyeKey);
+
+				token._googly_sptsEyes = token._googly_sptsEyes || {};
+
+				let sptEye = token._googly_sptsEyes[eyeKey];
+				if (sptEye && sptEye.parent) return;
+				if (sptEye && !sptEye.parent) token.removeChild(sptEye);
+
+				sptEye = new PIXI.Sprite(GooglyEyes._TEXTURE);
+
+				sptEye.anchor.set(0.5, 0.5);
+				sptEye.position.set(pt.x, pt.y);
+				sptEye.width = sptEye.height = pt.dim;
+				sptEye.angle = pt.angle;
+
+				token._googly_sptsEyes[eyeKey] = sptEye;
+
+				token.addChild(sptEye);
+			});
+
+		if (!token._googly_sptsEyes) return;
+		Object.entries(token._googly_sptsEyes)
+			.forEach(([k, v]) => {
+				if (seenKeys.has(k)) return;
+				delete token._googly_sptsEyes[k];
+				token.removeChild(v);
+			});
+	}
+
+	static _doHandleClickLeft (token, evt) {
+		if (!GooglyEyes._IS_ACTIVE) return false;
+
+		evt.stopPropagation();
+
+		const points = GooglyEyes._getTokenPoints(token);
+		const {x, y} = GooglyEyes._getTokenSpaceClickPosition(token, evt);
+
+		points.push({
+			x,
+			y,
+			dim: Math.round(20 + Math.random() * 16),
+			angle: Math.random() * 360,
+		});
+
+		token.update({flags: {[GooglyEyes._MODULE_NAME]: {points}}})
+			.then(() => {
+				token.refresh();
+			});
+
+		return true;
+	}
+
+	static _doHandleClickRight (token, evt) {
+		if (!GooglyEyes._IS_ACTIVE) return false;
+
+		evt.stopPropagation();
+
+		const points = GooglyEyes._getTokenPoints(token);
+		const {x, y} = GooglyEyes._getTokenSpaceClickPosition(token, evt);
+
+		// Loop backwards through the points, looking for collisions
+		for (let i = points.length - 1; i > -1; --i) {
+			const pt = points[i];
+
+			if (
+				x < (pt.x - (pt.dim * 0.5))
+				|| x > (pt.x + (pt.dim * 0.5))
+				|| y < (pt.y - (pt.dim * 0.5))
+				|| y > (pt.y + (pt.dim * 0.5))
+			) {
+				continue;
+			}
+
+			points.splice(i, 1);
+
+			token.update({flags: {[GooglyEyes._MODULE_NAME]: {points}}})
+				.then(() => {
+					token.refresh();
+				});
+
+			break;
+		}
+
+		return true;
+	}
+
+	static _getTokenSpaceClickPosition (token, evt) {
+		const {x: xClick, y: yClick} = evt.data.origin;
+		const {x: xToken, y: yToken} = token.position;
+		return {
+			x: xClick - xToken,
+			y: yClick - yToken,
+		};
+	}
+
+	static _getTokenPoints (token) {
+		return JSON.parse(JSON.stringify(token.data.flags?.[GooglyEyes._MODULE_NAME]?.points || []));
+	}
+}
+GooglyEyes._TEXTURE = null;
+GooglyEyes._IS_ACTIVE = false;
+GooglyEyes._MODULE_NAME = "googly-eyes";
+
+Hooks.on("ready", () => {
+	GooglyEyes.pInit();
+});
